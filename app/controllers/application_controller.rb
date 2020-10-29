b982f338917693c01aefb3b4ec18671794d4ad2f
Jocyline Magero <jmagero2017@gmail.com>
2020-10-29 06:38:28 +0300

    Finished static_pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
 
-    def helo
-        render html: 'helo world'
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'application#helo'
+  root to: 'static_pages#home'
 end
