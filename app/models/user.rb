9bf0496d1257e06099dbf059e0250e1ba759cc9c
Jocyline Magero <jmagero2017@gmail.com>
2020-11-09 19:21:46 +0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: {maximum: 255},
                                     format: {with: VALID_EMAIL_REGEX},
-                                    uniquiness: { case_sensitive: false}
+                                    uniqueness: true
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
