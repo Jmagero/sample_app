4f1e22df85c0f21bc9480fa0e31149398f6b3556
Jocyline Magero <jmagero2017@gmail.com>
2020-11-09 23:02:45 +0300

    Use SSL and the puma webserver in production

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,5 +3,5 @@ module UsersHelper
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
         gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}"
         image_tag(gravatar_url, alt: user.name, class: "gravatar")
-      end
+    end
 end
